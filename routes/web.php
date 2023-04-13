6d07d259736c75c50292692dac3a68886934811d
rambabu.g <rambabu.g@whatarage.com>
2023-04-13 18:24:49 +0530

    jetstream installed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 
 Route::get("/test",[HomeController::class,"index"]);
 Route::get("/contact",[HomeController::class,"index"]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
