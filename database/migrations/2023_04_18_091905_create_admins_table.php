3a23e5f716e7aee2baa5d72610d8c3b94cf5577f
rambabu.g <rambabu.g@whatarage.com>
2023-04-18 19:14:50 +0530

    multi auth admin login added in jet stream

--- database/migrations/2023_04_18_091905_create_admins_table.php
+++ database/migrations/2023_04_18_091905_create_admins_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('_admin', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_admin');
+        Schema::dropIfExists('admins');
     }
 };
